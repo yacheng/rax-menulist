c99b68a041d5cba7365f8e67ae09580f7cd9264f
yacheng.sz <yacheng.sz@taobao.com>
2019-01-17 16:11:04 +0800

    feat: test

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,9 +1,9 @@
 import {createElement, Component} from 'rax';
 import renderer from 'rax-test-renderer';
-import BarCode from '../';
+import Menu from '../';
 
-describe('BarCode', () => {
-  it('test typeof BarCode', () => {
-    expect(typeof BarCode).toEqual('function');
+describe('Menu', () => {
+  it('test typeof Menu', () => {
+    expect(typeof Menu).toEqual('function');
   });
 });
